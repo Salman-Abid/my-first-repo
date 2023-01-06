b54151bda507a03942a1f68ed870bca50ea5be1a
Salman-Abid <abidsalman389@gmail.com>
2023-01-06 18:20:16 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+
+var obj1 = {
+    a: 10,
+    b: 20,
+    c: 30,
+    d: 40,
+    e: 50,
+}
+
+
+console.log(Object.keys(obj1))
+console.log(Object.values(obj1))
+console.log(Object.entries(obj1))
+
+
